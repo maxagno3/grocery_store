b02b8ee065c1b33911d6800da1b34c24a44ae7c0
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-12-13 17:45:11 +0530

    add take input from user

--- main.rb
+++
@@ -1,3 +0,0 @@
-def main
-  p "Welcome to Grocery Store."
-end
---
+++ take_input.rb
@@ -0,0 +1,29 @@
+class Cart
+  def initialize
+    @order = Hash.new(0)
+  end
+
+  def take_input
+    puts "Please enter all the items purchased separated by a comma"
+
+    user_input = gets.chomp.downcase.gsub(/\s+/, "").split(",")
+
+    if user_input.length < 1
+      puts "Invalid order! Try again."
+      take_input
+    else
+      user_input.each { |order| @order[order] += 1 }
+      
+      puts "Order for the following has been confirmed. Thank you.\n"
+
+      
+    end
+  end
+
+  def view_order
+    print @order
+  end
+end
+
+cart_items = Cart.new
+cart_items.take_input
